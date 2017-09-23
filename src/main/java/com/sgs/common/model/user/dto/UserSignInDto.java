e429c5d852de218f3218a01b2bd22816a424f84b
Rob Kovtiuk <robsonheater@gmail.com>
2017-09-23 15:59:11 +0300

    User common data (fix userSignInDto)

--- src/main/java/com/sgs/common/model/user/dto/UserSignInDto.java
+++ src/main/java/com/sgs/common/model/user/dto/UserSignInDto.java
@@ -5,8 +5,16 @@ public class UserSignInDto {
     private String email;
     private String password;
 
+    public String getEmail() {
+        return email;
+    }
+
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public void setPassword(String password) {
